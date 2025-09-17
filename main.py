33d1a16d2fb150b0a8ea02ff62cd8af3ea67aa21
Aniya-star14 <aniyasandoval@icloud.com>
2025-09-17 17:41:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def get_user_name():
     return input("Please enter your name: ")
 
 def greet_user(name):
-    print(f"Hello, {name}! I am bryce the developer.")
+    print(f"Hello, {name}! I am Aniya the Reviewer.")
 
 def main():
     user_name = get_user_name()
     greet_user(user_name)
 
 if __name__ == "__main__":
-    main()
+    main() 
